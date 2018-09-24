30b95d0f9dfc776ba472874d185193acc5b031d3
Ugo Plouviez <up@iex.ec>
2018-09-24 16:30:03 +0200

    First commit

---
+++ src/main/java/com/iexec/worker/iexecworkernew/IexecWorkerNewApplication.java
@@ -0,0 +1,12 @@
+package com.iexec.worker.iexecworkernew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexecWorkerNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IexecWorkerNewApplication.class, args);
+	}
+}
---
+++ src/test/java/com/iexec/worker/iexecworkernew/IexecWorkerNewApplicationTests.java
@@ -0,0 +1,16 @@
+package com.iexec.worker.iexecworkernew;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IexecWorkerNewApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
