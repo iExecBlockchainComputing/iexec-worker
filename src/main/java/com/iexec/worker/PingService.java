22d1cbef12bff9314976c593842eb54bd599a114
Ugo Plouviez <up@iex.ec>
2018-10-02 17:07:17 +0200

    Remove hard coded value

--- src/main/java/com/iexec/worker/PingService.java
+++ src/main/java/com/iexec/worker/PingService.java
@@ -1,6 +1,7 @@
 package com.iexec.worker;
 
 import com.iexec.worker.feign.CoreClient;
+import com.iexec.worker.utils.WorkerConfigurationService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
@@ -10,14 +11,17 @@ import org.springframework.stereotype.Service;
 public class PingService {
 
     private CoreClient coreClient;
+    private WorkerConfigurationService workerConfService;
 
-    public PingService(CoreClient coreClient){
+    public PingService(CoreClient coreClient,
+                       WorkerConfigurationService workerConfService) {
         this.coreClient = coreClient;
+        this.workerConfService = workerConfService;
     }
 
     @Scheduled(fixedRate = 10000)
     public void pingScheduler() {
         log.info("try to ping scheduler");
-        coreClient.ping("customWorker");
+        coreClient.ping(workerConfService.getWorkerName());
     }
 }
