985743c0b5e7aa7456bb9c686a103fd5300fbe28
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2018-10-02 14:23:25 +0200

    Worker able to execute docker tasks

--- src/main/java/com/iexec/worker/task/TaskService.java
+++ src/main/java/com/iexec/worker/task/TaskService.java
@@ -1,12 +1,17 @@
 package com.iexec.worker.task;
 
-import com.iexec.common.replicate.Replicate;
+import com.iexec.common.dapp.DappType;
+import com.iexec.common.replicate.ReplicateModel;
 import com.iexec.common.replicate.ReplicateStatus;
+import com.iexec.worker.docker.DockerService;
 import com.iexec.worker.feign.CoreClient;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class TaskService {
 
@@ -14,27 +19,39 @@ public class TaskService {
     private String workerName;
 
     private CoreClient coreClient;
+    private DockerService dockerService;
 
-    public TaskService(CoreClient coreClient) {
+    @Autowired
+    public TaskService(CoreClient coreClient, DockerService dockerService) {
         this.coreClient = coreClient;
+        this.dockerService = dockerService;
     }
 
     @Scheduled(fixedRate = 30000)
     public String getTask() {
-        Replicate replicate = coreClient.getReplicate(workerName);
-        if (replicate == null || replicate.getTaskId() == null){
+        ReplicateModel replicateModel = coreClient.getReplicate(workerName);
+        log.info("Getting task [taskId:{}]", replicateModel.getTaskId());
+        if (replicateModel == null || replicateModel.getTaskId() == null){
             return "NO TASK AVAILABLE";
         }
 
-        coreClient.updateReplicateStatus(replicate.getTaskId(), ReplicateStatus.RUNNING, workerName);
+        coreClient.updateReplicateStatus(replicateModel.getTaskId(), ReplicateStatus.RUNNING, workerName);
 
-        // simulate some work on the task
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        if (replicateModel.getDappType().equals(DappType.DOCKER)){
+            dockerService.dockerRun(replicateModel.getDappName(), replicateModel.getCmd());
+        } else {
+            // simulate some work on the task
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-        coreClient.updateReplicateStatus(replicate.getTaskId(), ReplicateStatus.COMPLETED, workerName);
+
+
+
+        coreClient.updateReplicateStatus(replicateModel.getTaskId(), ReplicateStatus.COMPLETED, workerName);
         return ReplicateStatus.COMPLETED.toString();
     }
 
