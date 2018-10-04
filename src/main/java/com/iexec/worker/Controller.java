bdb854f522239b95694a873eb93ad30ac54f436c
Ugo Plouviez <up@iex.ec>
2018-10-04 10:07:26 +0200

    Modify feign client

--- src/main/java/com/iexec/worker/Controller.java
+++ src/main/java/com/iexec/worker/Controller.java
@@ -1,14 +1,10 @@
 package com.iexec.worker;
 
 
-import com.iexec.common.replicate.ReplicateModel;
-import com.iexec.common.replicate.ReplicateStatus;
 import com.iexec.worker.docker.ContainerResult;
 import com.iexec.worker.docker.DockerService;
-import com.iexec.worker.feign.CoreTaskClient;
-import com.iexec.worker.utils.WorkerConfigurationService;
+import com.iexec.worker.task.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,38 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-    private CoreTaskClient coreTaskClient;
-
+    private TaskService taskService;
     private DockerService dockerService;
-    private WorkerConfigurationService workerConfigurationService;
 
     @Autowired
-    public Controller(CoreTaskClient coreTaskClient,
-                      DockerService dockerService,
-                      WorkerConfigurationService workerConfigurationService) {
-        this.coreTaskClient = coreTaskClient;
+    public Controller(DockerService dockerService,
+                      TaskService taskService) {
         this.dockerService = dockerService;
-        this.workerConfigurationService = workerConfigurationService;
+        this.taskService = taskService;
+
     }
 
     @GetMapping("/getTask")
     public String getTask() {
-        String workerName = workerConfigurationService.getWorkerName();
-        ReplicateModel replicate = coreTaskClient.getReplicate(workerName).getBody();
-        if (replicate.getTaskId() == null) {
-            return "NO TASK AVAILABLE";
-        }
-
-        coreTaskClient.updateReplicateStatus(replicate.getTaskId(), ReplicateStatus.RUNNING, workerName);
-
-        // simulate some work on the task
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        coreTaskClient.updateReplicateStatus(replicate.getTaskId(), ReplicateStatus.COMPLETED, workerName);
-        return ReplicateStatus.COMPLETED.toString();
+        return taskService.getTask();
     }
 
     //http://localhost:18091/docker/run?image=iexechub/vanityeth:latest&cmd=a
