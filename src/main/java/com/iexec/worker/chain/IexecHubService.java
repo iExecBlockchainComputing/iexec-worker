3853d58119f9cf1f54bdb7b49157223f8c002160
Ugo Plouviez <up@iex.ec>
2018-11-08 11:51:36 +0100

    Use chain utils from common

--- src/main/java/com/iexec/worker/chain/IexecHubService.java
+++ src/main/java/com/iexec/worker/chain/IexecHubService.java
@@ -1,20 +1,15 @@
 package com.iexec.worker.chain;
 
 
+import com.iexec.common.chain.ChainUtils;
 import com.iexec.common.contract.generated.IexecHubABILegacy;
 import com.iexec.worker.feign.CoreWorkerClient;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.web3j.crypto.Credentials;
-import org.web3j.ens.EnsResolutionException;
-import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.DefaultBlockParameterName;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
-import org.web3j.protocol.http.HttpService;
-import org.web3j.tx.gas.DefaultGasProvider;
 
-import java.io.IOException;
 import java.util.List;
 
 
@@ -26,9 +21,9 @@ public class IexecHubService {
 
     @Autowired
     public IexecHubService(CredentialsService credentialsService, CoreWorkerClient coreWorkerClient) {
-        iexecHub = loadHubContract(
+        iexecHub = ChainUtils.loadHubContract(
                 credentialsService.getCredentials(),
-                getWeb3j(coreWorkerClient.getPublicConfiguration().getBlockchainURL()),
+                ChainUtils.getWeb3j(coreWorkerClient.getPublicConfiguration().getBlockchainURL()),
                 coreWorkerClient.getPublicConfiguration().getIexecHubAddress());
 
         startWatchers();
@@ -47,39 +42,6 @@ public class IexecHubService {
 
     }
 
-    private IexecHubABILegacy loadHubContract(Credentials credentials, Web3j web3j, String iexecHubAddress) {
-        ExceptionInInitializerError exceptionInInitializerError = new ExceptionInInitializerError("Failed to load IexecHub contract from address " + iexecHubAddress);
-
-        if (iexecHubAddress != null && !iexecHubAddress.isEmpty()) {
-            try {
-                IexecHubABILegacy iexecHub = IexecHubABILegacy.load(
-                        iexecHubAddress, web3j, credentials, new DefaultGasProvider());
-                //if (!iexecHub.isValid()){ throw exceptionInInitializerError;}
-
-                log.info("Loaded contract IexecHub [address:{}] ", iexecHubAddress);
-                return iexecHub;
-            } catch (EnsResolutionException e) {
-                throw exceptionInInitializerError;
-            }
-        } else {
-            throw exceptionInInitializerError;
-        }
-    }
-
-    private Web3j getWeb3j(String chainNodeAddress) {
-        Web3j web3j = Web3j.build(new HttpService(chainNodeAddress));
-        ExceptionInInitializerError exceptionInInitializerError = new ExceptionInInitializerError("Failed to connect to ethereum node " + chainNodeAddress);
-        try {
-            log.info("Connected to Ethereum node [node:{}]", web3j.web3ClientVersion().send().getWeb3ClientVersion());
-            if (web3j.web3ClientVersion().send().getWeb3ClientVersion() == null) {
-                throw exceptionInInitializerError;
-            }
-        } catch (IOException e) {
-            throw exceptionInInitializerError;
-        }
-        return web3j;
-    }
-
     private void startWatchers() {
         iexecHub.workerSubscriptionEventObservable(DefaultBlockParameterName.EARLIEST, DefaultBlockParameterName.LATEST)
                 .subscribe(workerSubscriptionEventResponse ->
@@ -92,7 +54,7 @@ public class IexecHubService {
             log.info("Subscribing to pool [pool:{}]", poolAddress);
             TransactionReceipt subscribeReceipt = iexecHub.subscribe(poolAddress).send();
             List<IexecHubABILegacy.WorkerSubscriptionEventResponse> workerSubscriptionEvents = iexecHub.getWorkerSubscriptionEvents(subscribeReceipt);
-            if (workerSubscriptionEvents != null && workerSubscriptionEvents.size() > 0) {
+            if (workerSubscriptionEvents != null && !workerSubscriptionEvents.isEmpty()) {
                 log.info("Subscribed to pool [pool:{}, worker:{}]", workerSubscriptionEvents.get(0).pool, workerSubscriptionEvents.get(0).worker);
             }
         } catch (Exception e) {
