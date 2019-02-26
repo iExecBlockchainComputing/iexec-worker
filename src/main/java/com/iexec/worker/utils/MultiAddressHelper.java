019d6e88165e8fe161b7ad3c730337a72a864732
Ugo Plouviez <up@iex.ec>
2019-02-26 14:14:02 +0100

    Manage IPFS addresses for the input dataset

---
+++ src/main/java/com/iexec/worker/utils/MultiAddressHelper.java
@@ -0,0 +1,25 @@
+package com.iexec.worker.utils;
+
+import com.iexec.common.utils.BytesUtils;
+import io.ipfs.multiaddr.MultiAddress;
+import org.web3j.utils.Numeric;
+
+public class MultiAddressHelper {
+
+    private static final String IPFS_GATEWAY_URI = "https://gateway.ipfs.io";
+
+    private MultiAddressHelper() {
+        throw new UnsupportedOperationException();
+    }
+
+    public static String convertToURI(String hexaString) {
+        // Currently, only the IPFS format is supported in the multiaddress field in the smart contract, so
+        // if the MultiAddress object can't be constructed, it means it is an HTTP address.
+        try {
+            MultiAddress address = new MultiAddress(Numeric.hexStringToByteArray(hexaString));
+            return IPFS_GATEWAY_URI + address.toString();
+        } catch (Exception e) {
+            return BytesUtils.hexStringToAscii(hexaString);
+        }
+    }
+}
