6948d6d8b3ed5d9bc904d6a786b9038a672a39a2
Zied <guesmy.zied@gmail.com>
2019-03-13 09:42:43 +0100

    Demand a replicate instead of task

--- src/main/java/com/iexec/worker/feign/ReplicateClient.java
+++ src/main/java/com/iexec/worker/feign/ReplicateClient.java
@@ -3,7 +3,6 @@ package com.iexec.worker.feign;
 import com.iexec.common.replicate.ReplicateDetails;
 import java.util.List;
 
-import com.iexec.common.chain.ChainReceipt;
 import com.iexec.common.chain.ContributionAuthorization;
 import com.iexec.common.disconnection.InterruptedReplicateModel;
 import com.iexec.common.replicate.ReplicateStatus;
@@ -15,13 +14,13 @@ import org.springframework.web.bind.annotation.*;
 @FeignClient(name = "ReplicateClient", url = "http://${core.host}:${core.port}")
 public interface ReplicateClient {
 
-    @GetMapping("/tasks/available")
+    @GetMapping("/replicates/available")
     ContributionAuthorization getAvailableReplicate(
             @RequestParam(name = "blockNumber") long blockNumber,
             @RequestHeader("Authorization") String bearerToken
     ) throws FeignException;
 
-    @GetMapping("/tasks/interrupted")
+    @GetMapping("/replicates/interrupted")
     List<InterruptedReplicateModel> getInterruptedReplicates(
             @RequestParam(name = "blockNumber") long blockNumber,
             @RequestHeader("Authorization") String bearerToken
