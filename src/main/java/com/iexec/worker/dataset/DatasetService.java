9914ec965ac931f27d66a6e7e87d4c7d1a953a96
Zied <guesmy.zied@gmail.com>
2019-04-15 14:57:11 +0100

    Implement decryptDataset logic

--- src/main/java/com/iexec/worker/dataset/DatasetService.java
+++ src/main/java/com/iexec/worker/dataset/DatasetService.java
@@ -1,42 +1,28 @@
 package com.iexec.worker.dataset;
 
-import com.iexec.common.chain.ContributionAuthorization;
-import com.iexec.common.security.Signature;
-import com.iexec.common.sms.SmsSecretRequest;
-import com.iexec.common.sms.SmsSecretResponse;
-import com.iexec.common.sms.SmsSecretResponse.TaskSecrets;
-import com.iexec.common.utils.BytesUtils;
-import com.iexec.common.utils.HashUtils;
-import com.iexec.worker.chain.CredentialsService;
 import com.iexec.worker.config.WorkerConfigurationService;
-import com.iexec.worker.feign.SmsClientWrapper;
+import com.iexec.worker.sms.SmsService;
 import com.iexec.worker.utils.FileHelper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.web3j.crypto.Sign;
 
 import java.io.BufferedReader;
 import java.io.File;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Optional;
+import java.nio.file.Paths;
+
 
 @Slf4j
 @Service
 public class DatasetService {
 
-    private static final String DATASET_SECRET_FILENAME = "dataset.secret";
+    private final WorkerConfigurationService workerConfigurationService;
+    private final SmsService smsService;
 
-    private final WorkerConfigurationService configurationService;
-    private final CredentialsService credentialsService;
-    private final SmsClientWrapper smsClientWrapper;
-
-    public DatasetService(WorkerConfigurationService configurationService,
-                          CredentialsService credentialsService,
-                          SmsClientWrapper smsClientWrapper) {
-        this.configurationService = configurationService;
-        this.credentialsService = credentialsService;
-        this.smsClientWrapper = smsClientWrapper;
+    public DatasetService(WorkerConfigurationService workerConfigurationService,
+                          SmsService smsService) {
+        this.workerConfigurationService = workerConfigurationService;
+        this.smsService = smsService;
     }
 
     /*
@@ -55,85 +41,55 @@ public class DatasetService {
                     chainTaskId, datasetUri);
             return true;
         }
-        return FileHelper.downloadFileInDirectory(datasetUri, getDatasetFolderPath(chainTaskId));
+        return FileHelper.downloadFileInDirectory(datasetUri, workerConfigurationService.getTaskInputDir(chainTaskId));
     }
 
-    public String getDatasetFolderPath(String chainTaskId) {
-        return configurationService.getResultBaseDir() + File.separator + chainTaskId + FileHelper.SLASH_INPUT;
-    }
+    public boolean decryptDataset(String chainTaskId, String datasetUri) {
+        String datasetFileName = Paths.get(datasetUri).getFileName().toString();
+        String datasetFilePath = workerConfigurationService.getTaskInputDir(chainTaskId) + File.separator + datasetFileName;
+        String datasetSecretFilePath = smsService.getDatasetSecretFilePath(chainTaskId);
 
-    public boolean decryptData(ContributionAuthorization contributionAuth) {
-
-        String hash = HashUtils.concatenateAndHash(contributionAuth.getWorkerWallet(),
-                contributionAuth.getChainTaskId(), contributionAuth.getEnclaveChallenge());
-
-        Sign.SignatureData workerSignature = Sign.signPrefixedMessage(
-                BytesUtils.stringToBytes(hash), credentialsService.getCredentials().getEcKeyPair());
-
-        SmsSecretRequest smsSecretRequest = SmsSecretRequest.builder()
-                .chainTaskId(contributionAuth.getChainTaskId())
-                .workerAddress(contributionAuth.getWorkerWallet())
-                .enclaveChallenge(contributionAuth.getEnclaveChallenge())
-                .coreSignature(contributionAuth.getSignature())
-                .workerSignature(new Signature(workerSignature))
-                .build();
-
-        Optional<SmsSecretResponse> oSmsSecretResponse = smsClientWrapper.getTaskSecrets(smsSecretRequest);
-
-        if (!oSmsSecretResponse.isPresent()) {
-            return false;
-        }
-
-        String chainTaskId = smsSecretRequest.getChainTaskId();
-        String datasetFolderPath = getDatasetFolderPath(chainTaskId);
-        SmsSecretResponse smsSecretResponse = oSmsSecretResponse.get();
-        if (smsSecretResponse.getData() == null) {
-            log.info("no secret was found, no need to decrypt data [chainTaskId:{}]", chainTaskId);
+        if (!new File(datasetSecretFilePath).exists()) {
+            log.info("No dataset secret found, no need to decrypt data [chainTaskId:{}]", chainTaskId);
             return true;
         }
 
-        TaskSecrets taskSecrets = smsSecretResponse.getData();
-        String datasetSecretFilePath = datasetFolderPath + File.separator + DATASET_SECRET_FILENAME;
-        String datasetFilePath = datasetFolderPath + File.separator + FileHelper.getDatasetFileName(datasetFolderPath);
+        log.info("Decrypting dataset file [datasetFile:{}, secretFile:{}]", datasetFilePath, datasetSecretFilePath);
 
-        if (datasetFilePath.isEmpty()) {
-            log.error("could not get dataset filename, file not found [chainTaskId:{}, ]");
+        decryptFile(datasetFilePath, datasetSecretFilePath);
+
+        String decryptedDatasetFilePath = datasetFilePath + ".recovered";
+
+        if (!new File(decryptedDatasetFilePath).exists()) {
+            log.error("Decrypted dataset file not found [chainTaskId:{}, decryptedDatasetFilePath:{}]",
+                    chainTaskId, decryptedDatasetFilePath);
             return false;
         }
 
-        FileHelper.createFileWithContent(datasetSecretFilePath, taskSecrets.getDatasetSecret().getSecret());
-
-        try {
-            return decryptFile(datasetFilePath, datasetSecretFilePath);
-        } catch (Exception e) {
-            log.error("error while trying to decrypt data [chainTaskId:{}, dataFile{}, secretFile:{}]",
-                    chainTaskId, datasetFilePath, datasetSecretFilePath);
-            e.printStackTrace();
-            return false;
-        }
+        FileHelper.deleteFile(datasetFilePath);
+        return FileHelper.renameFile(decryptedDatasetFilePath, datasetFilePath);
     }
 
-    public boolean decryptFile(String dataFilePath, String secretFilePath) throws IOException, InterruptedException {
+    public void decryptFile(String dataFilePath, String secretFilePath) {
         String cmd = String.format("./decrypt-dataset.sh %s %s", dataFilePath, secretFilePath);
 
         ProcessBuilder pb = new ProcessBuilder(cmd.split(" "));
         pb.directory(new File("./src/main/resources/"));
-        Process pr = pb.start();
 
-        BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
-        String line;
-        boolean isDecrypted = false;
+        try {
+            Process pr = pb.start();
 
-        while ((line = in.readLine()) != null) {
-            log.info(line);
-            if (line.contains("decrypted file with success")) {
-                isDecrypted = true;
-            }
+            BufferedReader in = new BufferedReader(new InputStreamReader(pr.getInputStream()));
+            String line;
+    
+            while ((line = in.readLine()) != null) { log.info(line); }
+    
+            pr.waitFor();
+            in.close();
+        } catch (Exception e) {
+            log.error("Error while trying to decrypt data [datasetFile{}, secretFile:{}]",
+                    dataFilePath, secretFilePath);
+            e.printStackTrace();
         }
-
-        pr.waitFor();
-        in.close();
-        return isDecrypted;
     }
-
 }
