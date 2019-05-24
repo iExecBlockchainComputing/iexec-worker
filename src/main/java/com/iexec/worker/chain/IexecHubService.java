d8251ae7ddb4fd1188f527d9dd2e65874b2c74bd
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2019-05-24 14:38:28 +0200

    Check sync node before canReveal

--- src/main/java/com/iexec/worker/chain/IexecHubService.java
+++ src/main/java/com/iexec/worker/chain/IexecHubService.java
@@ -212,18 +212,6 @@ public class IexecHubService extends IexecHubAbstractService {
         return isChainTaskStatusActive;
     }
 
-    boolean isChainTaskRevealingWhenNodeNotSync(String chainTaskId, long consensusReachedBlockNumber) {
-        boolean isChainTaskStatusRevealing = false;
-
-        if (web3jService.isBlockAvailable(consensusReachedBlockNumber)) {
-            isChainTaskStatusRevealing = isChainTaskRevealing(chainTaskId);
-        }
-
-        if (!isChainTaskStatusRevealing){
-            log.error("ChainTask status is still not in 'revealing' stage after maxWaitingTime [chainTaskId:{}]", chainTaskId);
-        }
-        return isChainTaskStatusRevealing;
-    }
 
     private Boolean isChainTaskActive(String chainTaskId){
         Optional<ChainTask> chainTask = getChainTask(chainTaskId);
@@ -244,7 +232,7 @@ public class IexecHubService extends IexecHubAbstractService {
         return false;
     }
 
-    private Boolean isChainTaskRevealing(String chainTaskId){
+    public Boolean isChainTaskRevealing(String chainTaskId){
         Optional<ChainTask> chainTask = getChainTask(chainTaskId);
         if (chainTask.isPresent()){
             switch (chainTask.get().getStatus()){
