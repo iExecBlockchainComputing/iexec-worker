4a7fa39ff5bae118a15b6a633c7112c0c254d544
Zied <guesmy.zied@gmail.com>
2019-06-12 20:19:29 +0100

    Check earlier if image is was pulled & build container config separately

--- src/main/java/com/iexec/worker/executor/ComputationService.java
+++ src/main/java/com/iexec/worker/executor/ComputationService.java
@@ -4,6 +4,7 @@ import com.iexec.common.chain.ContributionAuthorization;
 import com.iexec.common.replicate.AvailableReplicateModel;
 import com.iexec.common.replicate.ReplicateStatus;
 import com.iexec.worker.dataset.DatasetService;
+import com.iexec.worker.docker.CustomDockerClient;
 import com.iexec.worker.docker.DockerComputationService;
 import com.iexec.worker.sms.SmsService;
 import com.iexec.worker.tee.scone.SconeTeeService;
@@ -22,18 +23,23 @@ import static com.iexec.common.replicate.ReplicateStatus.*;
 @Service
 public class ComputationService {
 
+    private static final String DATASET_FILENAME = "DATASET_FILENAME";
+
     private SmsService smsService;
     private DatasetService datasetService;
+    private CustomDockerClient customDockerClient;
     private DockerComputationService dockerComputationService;
     private SconeTeeService sconeTeeService;
 
     public ComputationService(SmsService smsService,
                               DatasetService datasetService,
+                              CustomDockerClient customDockerClient,
                               DockerComputationService dockerComputationService,
                               SconeTeeService sconeTeeService) {
 
         this.smsService = smsService;
         this.datasetService = datasetService;
+        this.customDockerClient = customDockerClient;
         this.dockerComputationService = dockerComputationService;
         this.sconeTeeService = sconeTeeService;
     }
@@ -72,6 +78,9 @@ public class ComputationService {
     public Pair<ReplicateStatus, String> runNonTeeComputation(AvailableReplicateModel replicateModel) {
         ContributionAuthorization contributionAuth = replicateModel.getContributionAuthorization();
         String chainTaskId = contributionAuth.getChainTaskId();
+        String imageUri = replicateModel.getAppUri();
+        String cmd = replicateModel.getCmd();
+        long maxExecutionTime = replicateModel.getMaxExecutionTime();
         String stdout = "";
 
         // fetch task secrets from SMS
@@ -96,7 +105,18 @@ public class ComputationService {
 
         // compute
         String datasetFilename = FileHelper.getFilenameFromUri(replicateModel.getDatasetUri());
-        stdout = dockerComputationService.dockerRunAndGetLogs(replicateModel, datasetFilename);
+        String env = DATASET_FILENAME + "=" + datasetFilename;
+
+        // stdout = dockerComputationService.dockerRunAndGetLogs(replicateModel, datasetFilename);
+        ContainerConfig containerConfig = customDockerClient.buildContainerConfig(chainTaskId, imageUri, cmd, env);
+
+        if (!customDockerClient.isImagePulled(imageUri)) {
+            stdout = "Application image not found, URI:" + imageUri;
+            log.error(stdout + " [chainTaskId:{}]", chainTaskId);
+            return Pair.of(COMPUTE_FAILED, stdout);
+        }
+
+        stdout = customDockerClient.dockerRun(chainTaskId, containerConfig, maxExecutionTime);
 
         if (stdout.isEmpty()) {
             stdout = "Failed to start computation";
