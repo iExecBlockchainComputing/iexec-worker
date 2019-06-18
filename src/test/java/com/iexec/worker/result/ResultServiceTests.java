ced7b332f1b540e7958f078a64c010b3d0d7f9ac
Zied <guesmy.zied@gmail.com>
2019-06-18 18:30:43 +0100

    Fix result service tests

--- src/test/java/com/iexec/worker/result/ResultServiceTests.java
+++ src/test/java/com/iexec/worker/result/ResultServiceTests.java
@@ -1,7 +1,6 @@
 package com.iexec.worker.result;
 
-import com.iexec.common.security.Signature;
-import com.iexec.common.utils.BytesUtils;
+import com.iexec.worker.chain.CredentialsService;
 import com.iexec.worker.config.WorkerConfigurationService;
 import com.iexec.worker.sms.SmsService;
 
@@ -11,9 +10,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import java.io.IOException;
-import java.util.Optional;
-
 import static org.assertj.core.api.Java6Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
@@ -22,6 +18,8 @@ public class ResultServiceTests {
     private final String IEXEC_WORKER_TMP_FOLDER = "./src/test/resources/tmp/test-worker";
 
     @Mock private WorkerConfigurationService configurationService;
+    @Mock private ResultRepoService resultRepoService;
+    @Mock private CredentialsService credentialsService;
     @Mock private SmsService smsService;
 
     @InjectMocks
@@ -51,32 +49,6 @@ public class ResultServiceTests {
         // should not be equal to the content of the file since it is not a byte32
         assertThat(hash).isNotEqualTo("dummyRandomString");
     }
-
-    @Test
-    public void shouldGetEnclaveSignature() throws IOException {
-        String chainTaskId = "1234";
-        String rExpected = "0x253554311f2793b72785a45eff4cbbe04c45d2e5a4d89057dfbc721e69b61d39";
-        String sExpected = "0x6bdf554c8c12c158d12f08299afbe0d9c8533bf420a5d3f63ed9827047eab8d1";
-        byte vExpected = 27;
-
-        when(configurationService.getTaskOutputDir(chainTaskId))
-                .thenReturn("./src/test/resources/tmp/test-worker/" + chainTaskId + "/output");
-        Optional<Signature> enclaveSignature = resultService.getEnclaveSignatureFromFile(chainTaskId);
-
-        assertThat(enclaveSignature.isPresent()).isTrue();
-        assertThat(enclaveSignature.get().getR()).isEqualTo(BytesUtils.stringToBytes(rExpected));
-        assertThat(enclaveSignature.get().getS()).isEqualTo(BytesUtils.stringToBytes(sExpected));
-        assertThat(enclaveSignature.get().getV()).isEqualTo(vExpected);
-    }
-
-    @Test
-    public void shouldNotGetEnclaveSignatureSinceFileMissing() throws IOException {
-        when(resultService.getResultFolderPath("chainTaskId")).thenReturn("./src/test/resources/fakefolder");
-        Optional<Signature> enclaveSignature = resultService.getEnclaveSignatureFromFile("chainTaskId");
-
-        assertThat(enclaveSignature.isPresent()).isFalse();
-    }
-
 
     @Test
     public void shouldGetCallbackDataFromFile(){
