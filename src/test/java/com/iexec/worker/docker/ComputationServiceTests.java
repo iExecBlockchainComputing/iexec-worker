c82562fa8f803f057cc99d08e5409c8fd1ba6753
Ugo Plouviez <up@iex.ec>
2019-07-25 15:31:10 +0200

    Minor change in unit test

--- src/test/java/com/iexec/worker/docker/ComputationServiceTests.java
+++ src/test/java/com/iexec/worker/docker/ComputationServiceTests.java
@@ -54,7 +54,7 @@ public class ComputationServiceTests {
                 .datasetUri("datasetUri")
                 .isTeeTask(isTeeTask)
                 .maxExecutionTime(500)
-                .cmd("rm -rf /*")
+                .cmd("ls")
                 .inputFiles(new ArrayList<>())
                 .build();
     }
