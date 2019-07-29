65c2c7192fd4822ce210c8bb1136ea62d2199e56
Zied <guesmy.zied@gmail.com>
2019-07-29 16:45:44 +0200

    Check wether the machine supports SGX or not

--- src/main/java/com/iexec/worker/tee/SgxService.java
+++ src/main/java/com/iexec/worker/tee/SgxService.java
@@ -2,6 +2,8 @@ package com.iexec.worker.tee;
 
 import java.io.File;
 
+import com.iexec.worker.docker.CustomDockerClient;
+import com.iexec.worker.docker.DockerExecutionConfig;
 import com.iexec.worker.utils.LoggingUtils;
 
 import org.springframework.stereotype.Service;
@@ -14,23 +16,46 @@ import lombok.extern.slf4j.Slf4j;
 public class SgxService {
 
     public static final String SGX_DEVICE_PATH = "/dev/isgx";
-    public static final String SGX_DRIVER_PATH = "/sys/module/isgx/version";
+    public static final String SGX_CGROUP_PERMISSIONS = "rwm";
+    private static final String SGX_DRIVER_PATH = "/sys/module/isgx/version";
+
+    private CustomDockerClient customDockerClient;
+
+    public SgxService(CustomDockerClient customDockerClient) {
+        this.customDockerClient = customDockerClient;
+    }
 
     public boolean isSgxEnabled() {
         boolean isSgxDriverFound = new File(SGX_DRIVER_PATH).exists();
-        boolean isSgxDeviceFound = new File(SGX_DEVICE_PATH).exists();
 
         if (!isSgxDriverFound) {
             log.debug("SGX driver not found");
             return false;
         }
 
-        if (isSgxDriverFound && !isSgxDeviceFound) {
+        boolean isSgxDeviceFound = isSgxDeviceFound();
+
+        if (!isSgxDeviceFound) {
             String message = "SGX driver is installed but no SGX device found. Please check if SGX is enabled";
             LoggingUtils.printHighlightedMessage(message);
             return false;
         }
 
         return true;
+    }
+
+    public boolean isSgxDeviceFound() {
+        String cmd = "ls /dev/isgx >/dev/null 2>1 && echo true || echo false";
+
+        DockerExecutionConfig dockerExecutionConfig = DockerExecutionConfig.builder()
+                .chainTaskId("sgxCheck")
+                .imageUri("alpine:lates")
+                .cmd(cmd.split(" "))
+                .containerName("sgxCheck")
+                .maxExecutionTime(300) // 5min
+                .build();
+
+        String stdout = customDockerClient.runSgxCheckContainer(dockerExecutionConfig);
+        return stdout != null && stdout.equals("true") ? true : false;
     }
 }
