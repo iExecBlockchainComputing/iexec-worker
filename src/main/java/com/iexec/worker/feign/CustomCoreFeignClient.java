7c7e85a179f2414d0502f4ee263f9748a9685224
Jérémy James Toussaint <jeremy.toussaint@iex.ec>
2021-02-22 15:12:47 +0100

    Removed code smells from custom core feign client

--- src/main/java/com/iexec/worker/feign/CustomCoreFeignClient.java
+++ src/main/java/com/iexec/worker/feign/CustomCoreFeignClient.java
@@ -34,6 +34,8 @@ import java.util.*;
 @Service
 public class CustomCoreFeignClient extends BaseFeignClient {
 
+    public static final String JWTOKEN = "jwtoken";
+    public static final String BLOCK_NUMBER = "blockNumber";
     private final LoginService loginService;
     private final CoreClient coreClient;
 
@@ -65,21 +67,21 @@ public class CustomCoreFeignClient extends BaseFeignClient {
      */
 
     public PublicConfiguration getPublicConfiguration() {
-        HttpCall<PublicConfiguration> httpCall = (args) -> coreClient.getPublicConfiguration();
+        HttpCall<PublicConfiguration> httpCall = args -> coreClient.getPublicConfiguration();
         ResponseEntity<PublicConfiguration> response = makeHttpCall(httpCall, null, "getPublicConfig");
         return is2xxSuccess(response) ? response.getBody() : null;
     }
 
     public String getCoreVersion() {
-        HttpCall<String> httpCall = (args) -> coreClient.getCoreVersion();
+        HttpCall<String> httpCall = args -> coreClient.getCoreVersion();
         ResponseEntity<String> response = makeHttpCall(httpCall, null, "getCoreVersion");
         return is2xxSuccess(response) ? response.getBody() : null;
     }
 
     public String ping() {
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put("jwtoken", loginService.getToken());
-        HttpCall<String> httpCall = (args) -> coreClient.ping((String) args.get("jwtoken"));
+        arguments.put(JWTOKEN, loginService.getToken());
+        HttpCall<String> httpCall = args -> coreClient.ping((String) args.get(JWTOKEN));
         ResponseEntity<String> response = makeHttpCall(httpCall, arguments, "ping");
         return is2xxSuccess(response) && response.getBody() != null ? response.getBody() : "";
     }
@@ -87,19 +89,19 @@ public class CustomCoreFeignClient extends BaseFeignClient {
     //TODO: Make registerWorker return Worker
     public boolean registerWorker(WorkerModel model) {
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put("jwtoken", loginService.getToken());
+        arguments.put(JWTOKEN, loginService.getToken());
         arguments.put("model", model);
-        HttpCall<Void> httpCall = (args) -> coreClient.registerWorker((String) args.get("jwtoken"), (WorkerModel) args.get("model"));
+        HttpCall<Void> httpCall = args -> coreClient.registerWorker((String) args.get(JWTOKEN), (WorkerModel) args.get("model"));
         ResponseEntity<Void> response = makeHttpCall(httpCall, arguments, "registerWorker");
         return is2xxSuccess(response);
     }
 
     public List<String> getComputingTasks() {
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put("jwtoken", loginService.getToken());
+        arguments.put(JWTOKEN, loginService.getToken());
 
         HttpCall<List<String>> httpCall = args ->
-                coreClient.getComputingTasks((String) args.get("jwtoken"));
+                coreClient.getComputingTasks((String) args.get(JWTOKEN));
 
         ResponseEntity<List<String>> response = makeHttpCall(httpCall, arguments, "getComputingTasks");
         return is2xxSuccess(response) ? response.getBody() : Collections.emptyList();
@@ -107,11 +109,11 @@ public class CustomCoreFeignClient extends BaseFeignClient {
 
     public List<TaskNotification> getMissedTaskNotifications(long lastAvailableBlockNumber) {
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put("jwtoken", loginService.getToken());
-        arguments.put("blockNumber", lastAvailableBlockNumber);
+        arguments.put(JWTOKEN, loginService.getToken());
+        arguments.put(BLOCK_NUMBER, lastAvailableBlockNumber);
 
-        HttpCall<List<TaskNotification>> httpCall = (args) ->
-                coreClient.getMissedTaskNotifications((String) args.get("jwtoken"), (long) args.get("blockNumber"));
+        HttpCall<List<TaskNotification>> httpCall = args ->
+                coreClient.getMissedTaskNotifications((String) args.get(JWTOKEN), (long) args.get(BLOCK_NUMBER));
 
         ResponseEntity<List<TaskNotification>> response = makeHttpCall(httpCall, arguments, "getMissedNotifications");
         return is2xxSuccess(response) ? response.getBody() : Collections.emptyList();
@@ -119,11 +121,11 @@ public class CustomCoreFeignClient extends BaseFeignClient {
 
     public Optional<WorkerpoolAuthorization> getAvailableReplicate(long lastAvailableBlockNumber) {
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put("jwtoken", loginService.getToken());
-        arguments.put("blockNumber", lastAvailableBlockNumber);
+        arguments.put(JWTOKEN, loginService.getToken());
+        arguments.put(BLOCK_NUMBER, lastAvailableBlockNumber);
 
-        HttpCall<WorkerpoolAuthorization> httpCall = (args) ->
-                coreClient.getAvailableReplicate((String) args.get("jwtoken"), (long) args.get("blockNumber"));
+        HttpCall<WorkerpoolAuthorization> httpCall = args ->
+                coreClient.getAvailableReplicate((String) args.get(JWTOKEN), (long) args.get(BLOCK_NUMBER));
 
         ResponseEntity<WorkerpoolAuthorization> response = makeHttpCall(httpCall, arguments, "getAvailableReplicate");
         if (!is2xxSuccess(response) || response.getBody() == null) {
@@ -135,12 +137,12 @@ public class CustomCoreFeignClient extends BaseFeignClient {
 
     public TaskNotificationType updateReplicateStatus(String chainTaskId, ReplicateStatusUpdate replicateStatusUpdate) {
         Map<String, Object> arguments = new HashMap<>();
-        arguments.put("jwtoken", loginService.getToken());
+        arguments.put(JWTOKEN, loginService.getToken());
         arguments.put("chainTaskId", chainTaskId);
         arguments.put("statusUpdate", replicateStatusUpdate);
 
-        HttpCall<TaskNotificationType> httpCall = (args) ->
-                coreClient.updateReplicateStatus((String) args.get("jwtoken"), (String) args.get("chainTaskId"),
+        HttpCall<TaskNotificationType> httpCall = args ->
+                coreClient.updateReplicateStatus((String) args.get(JWTOKEN), (String) args.get("chainTaskId"),
                         (ReplicateStatusUpdate) args.get("statusUpdate"));
 
         ResponseEntity<TaskNotificationType> response = makeHttpCall(httpCall, arguments, "updateReplicateStatus");
