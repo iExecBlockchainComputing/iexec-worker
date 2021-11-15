ca47819eeda1a384c7b3c6ee577c1f05dfebaa6a
maxence <maxence.cornaton@iex.ec>
2021-11-15 09:05:58 +0100

    Remove unused imports

--- src/test/java/com/iexec/worker/config/BlockchainAdapterConfigurationServiceTest.java
+++ src/test/java/com/iexec/worker/config/BlockchainAdapterConfigurationServiceTest.java
@@ -3,9 +3,7 @@ package com.iexec.worker.config;
 import com.iexec.worker.feign.CustomBlockchainAdapterClient;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.time.Duration;
