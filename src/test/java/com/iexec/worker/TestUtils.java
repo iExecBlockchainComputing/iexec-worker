8822ffbb89d5bf4df27b34b2ca5f04d55bce84da
Maxence Cornaton <maxence.cornaton@iex.ec>
2022-09-16 17:04:57 +0200

    Restore `TestUtils`

--- src/test/java/com/iexec/worker/TestUtils.java
+++ src/test/java/com/iexec/worker/TestUtils.java
@@ -18,8 +18,6 @@ package com.iexec.worker;
 
 import org.mockito.invocation.InvocationOnMock;
 
-import java.util.concurrent.TimeUnit;
-
 public class TestUtils {
 
     public static class ThreadNameWrapper {
@@ -30,7 +28,7 @@ public class TestUtils {
             ThreadNameWrapper threadNameWrapper,
             InvocationOnMock invocation, int sleepDuration) throws Throwable {
         Object invocationResult = saveThreadNameThenCallRealMethod(threadNameWrapper, invocation);
-        TimeUnit.MILLISECONDS.sleep(sleepDuration);
+        Thread.sleep(sleepDuration);
         return invocationResult;
     }
 
@@ -38,21 +36,8 @@ public class TestUtils {
             ThreadNameWrapper threadNameWrapper, InvocationOnMock invocation)
             throws Throwable {
         // Save the name of the current thread
-        saveThreadName(threadNameWrapper);
+        threadNameWrapper.value = Thread.currentThread().getName();
         // Then call real method
         return invocation.callRealMethod();
-    }
-
-    public static Void saveThreadNameThenSleepSomeMillis(
-            ThreadNameWrapper threadNameWrapper, int sleepDuration) throws Throwable {
-        saveThreadName(threadNameWrapper);
-        TimeUnit.MILLISECONDS.sleep(sleepDuration);
-        return null;
-    }
-
-    public static Void saveThreadName(ThreadNameWrapper threadNameWrapper) {
-        // Save the name of the current thread
-        threadNameWrapper.value = Thread.currentThread().getName();
-        return null;
     }
 }
