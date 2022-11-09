1a3a5037cd6579e007f9ada4a27ab7e2ae554e0a
Jeremy Bernard <jeremy.bernard@iex.ec>
2022-11-09 18:20:33 +0100

    Rename VersionServiceTest to VersionServiceTests

--- src/test/java/com/iexec/worker/utils/version/VersionServiceTests.java
+++ src/test/java/com/iexec/worker/utils/version/VersionServiceTests.java
@@ -11,7 +11,7 @@ import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.boot.info.BuildProperties;
 
-class VersionServiceTest {
+class VersionServiceTests {
 
     @Mock
     private BuildProperties buildProperties;
