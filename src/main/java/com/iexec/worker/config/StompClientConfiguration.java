cfb7f6141beebeff5a2bc9b7bf2b0beff9c241d7
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-01-23 16:17:18 +0100

    Totally remove `SockJS` usage

--- src/main/java/com/iexec/worker/config/StompClientConfiguration.java
+++ src/main/java/com/iexec/worker/config/StompClientConfiguration.java
@@ -20,27 +20,16 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
 import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
-import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.SockJsClient;
-import org.springframework.web.socket.sockjs.client.Transport;
-import org.springframework.web.socket.sockjs.client.WebSocketTransport;
-
-import java.util.List;
 
 @Configuration
 public class StompClientConfiguration {
     @Bean
-    WebSocketStompClient stompClient(RestTemplate restTemplate) {
+    WebSocketStompClient stompClient() {
         final WebSocketClient webSocketClient = new StandardWebSocketClient();
-        final List<Transport> webSocketTransports = List.of(
-                new WebSocketTransport(webSocketClient)
-        );
-        final SockJsClient sockJsClient = new SockJsClient(webSocketTransports);
-        // without SockJS: new WebSocketStompClient(webSocketClient)
-        final WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
+        final WebSocketStompClient stompClient = new WebSocketStompClient(webSocketClient);
         stompClient.setAutoStartup(true);
         stompClient.setMessageConverter(new MappingJackson2MessageConverter());
         stompClient.setTaskScheduler(new ConcurrentTaskScheduler());
