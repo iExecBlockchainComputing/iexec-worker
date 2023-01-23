82702783fc4b6ccb3e7c35b84a908ae649490001
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-01-23 16:12:54 +0100

    Remove `SockJS` usage

--- src/main/java/com/iexec/worker/config/StompClientConfiguration.java
+++ src/main/java/com/iexec/worker/config/StompClientConfiguration.java
@@ -24,12 +24,10 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.socket.client.WebSocketClient;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
-import org.springframework.web.socket.sockjs.client.RestTemplateXhrTransport;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
@@ -37,9 +35,8 @@ public class StompClientConfiguration {
     @Bean
     WebSocketStompClient stompClient(RestTemplate restTemplate) {
         final WebSocketClient webSocketClient = new StandardWebSocketClient();
-        final List<Transport> webSocketTransports = Arrays.asList(
-                new WebSocketTransport(webSocketClient),
-                new RestTemplateXhrTransport(restTemplate)
+        final List<Transport> webSocketTransports = List.of(
+                new WebSocketTransport(webSocketClient)
         );
         final SockJsClient sockJsClient = new SockJsClient(webSocketTransports);
         // without SockJS: new WebSocketStompClient(webSocketClient)
