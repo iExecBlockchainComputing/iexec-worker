ac450bec8b4f4f8e9d3bf1e9ded9a44553fedcc3
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-02-28 10:13:13 +0100

    Extract Stomp client configuration to a new class

---
+++ src/test/java/com/iexec/worker/config/StompClientConfigurationTests.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 IEXEC BLOCKCHAIN TECH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.iexec.worker.config;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+import org.springframework.scheduling.concurrent.ConcurrentTaskScheduler;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.socket.messaging.WebSocketStompClient;
+import org.springframework.web.socket.sockjs.client.SockJsClient;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StompClientConfigurationTests {
+    @Mock
+    RestTemplate restTemplate;
+
+    StompClientConfiguration stompClientConfiguration = new StompClientConfiguration();
+
+    @BeforeEach
+    void init() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void shouldCreateStompClient() {
+        final WebSocketStompClient stompClient = stompClientConfiguration.stompClient(restTemplate);
+        assertAll(
+                () -> assertThat(stompClient).isNotNull(),
+                () -> assertThat(stompClient).extracting(WebSocketStompClient::isAutoStartup).isEqualTo(true),
+                () -> assertThat(stompClient).extracting(WebSocketStompClient::getMessageConverter).isInstanceOf(MappingJackson2MessageConverter.class),
+                () -> assertThat(stompClient).extracting(WebSocketStompClient::getTaskScheduler).isInstanceOf(ConcurrentTaskScheduler.class),
+                () -> assertThat(stompClient).extracting(WebSocketStompClient::getWebSocketClient).isInstanceOf(SockJsClient.class)
+        );
+    }
+}
