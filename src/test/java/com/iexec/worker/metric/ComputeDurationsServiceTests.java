3f4f4442cd4586c39740879dccb239588a51ae39
Maxence Cornaton <maxence.cornaton@iex.ec>
2023-11-20 11:00:15 +0100

    Improve `ComputeDurationsService` tests

--- src/test/java/com/iexec/worker/metric/ComputeDurationsServiceTests.java
+++ src/test/java/com/iexec/worker/metric/ComputeDurationsServiceTests.java
@@ -21,6 +21,7 @@ import io.micrometer.core.instrument.MeterRegistry;
 import io.micrometer.core.instrument.Metrics;
 import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -28,6 +29,8 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -39,38 +42,40 @@ class ComputeDurationsServiceTests {
     private static final String EXPORTED_STAT_PREFIX = "iexec_";
     private static final String CONTEXT = "test";
     private static final int STATISTICS_WINDOW = 100;
+    private static final List<Double> LONG_LIST_OF_VALUES = IntStream.range(0, STATISTICS_WINDOW + 1).mapToObj(i -> (double) i).collect(Collectors.toList());
 
-    private MeterRegistry meterRegistry;
     private ComputeDurationsService computeDurationsService;
     private Gauge minGauge;
     private Gauge maxGauge;
     private Gauge averageGauge;
+    private Gauge countGauge;
 
     @BeforeEach
     void beforeEach() {
-        meterRegistry = new SimpleMeterRegistry();
+        MeterRegistry meterRegistry = new SimpleMeterRegistry();
         Metrics.globalRegistry.add(meterRegistry);
+
         computeDurationsService = new ComputeDurationsService(meterRegistry, WALLET_ADDRESS, CONTEXT, STATISTICS_WINDOW);
         minGauge = meterRegistry.find(EXPORTED_STAT_PREFIX + CONTEXT + "_duration_min").gauge();
         maxGauge = meterRegistry.find(EXPORTED_STAT_PREFIX + CONTEXT + "_duration_max").gauge();
         averageGauge = meterRegistry.find(EXPORTED_STAT_PREFIX + CONTEXT + "_duration_average").gauge();
+        countGauge = meterRegistry.find(EXPORTED_STAT_PREFIX + CONTEXT + "_duration_count").gauge();
     }
 
     @AfterEach
     void afterEach() {
-        meterRegistry.clear();
         Metrics.globalRegistry.clear();
     }
 
     // region Constructor
     @Test
     void shouldRegisterGauges() {
-        assertThat(meterRegistry.find("task.duration.min.test"))
-                .isNotNull();
-        assertThat(meterRegistry.find("task.duration.max.test"))
-                .isNotNull();
-        assertThat(meterRegistry.find("task.duration.average.test"))
-                .isNotNull();
+        Assertions.assertAll(
+                () -> assertThat(minGauge).isNotNull(),
+                () -> assertThat(maxGauge).isNotNull(),
+                () -> assertThat(averageGauge).isNotNull(),
+                () -> assertThat(countGauge).isNotNull()
+        );
     }
     // endregion
 
@@ -93,10 +98,11 @@ class ComputeDurationsServiceTests {
         assertThat(computeDurationsService.getChainTaskIds()).isEmpty();
         computeDurationsService.addDurationForTask(CHAIN_TASK_ID_1, duration_1);
         computeDurationsService.addDurationForTask(CHAIN_TASK_ID_2, duration_2);
-        assertThat(computeDurationsService.getDurationForTask(CHAIN_TASK_ID_1))
-                .contains(duration_1);
-        assertThat(computeDurationsService.getDurationForTask(CHAIN_TASK_ID_2))
-                .contains(duration_2);
+
+        Assertions.assertAll(
+                () -> assertThat(computeDurationsService.getDurationForTask(CHAIN_TASK_ID_1)).contains(duration_1),
+                () -> assertThat(computeDurationsService.getDurationForTask(CHAIN_TASK_ID_2)).contains(duration_2)
+        );
     }
     // endregion
 
@@ -104,9 +110,12 @@ class ComputeDurationsServiceTests {
     @Test
     void shouldGetNanAggregatedDurationsSinceNoValue() {
         final AggregatedDurations aggregatedDurations = computeDurationsService.getAggregatedDurations();
-        assertThat(aggregatedDurations.getMinDuration()).isNaN();
-        assertThat(aggregatedDurations.getMaxDuration()).isNaN();
-        assertThat(aggregatedDurations.getAverageDuration()).isNaN();
+        Assertions.assertAll(
+                () -> assertThat(aggregatedDurations.getMinDuration()).isNaN(),
+                () -> assertThat(aggregatedDurations.getMaxDuration()).isNaN(),
+                () -> assertThat(aggregatedDurations.getAverageDuration()).isNaN(),
+                () -> assertThat(aggregatedDurations.getDurationsCount()).isZero()
+        );
     }
 
     @Test
@@ -116,9 +125,8 @@ class ComputeDurationsServiceTests {
         computeDurationsService.addDurationForTask(CHAIN_TASK_ID_1, duration);
 
         final AggregatedDurations aggregatedDurations = computeDurationsService.getAggregatedDurations();
-        assertThat(aggregatedDurations.getMinDuration()).isEqualTo(duration);
-        assertThat(aggregatedDurations.getMaxDuration()).isEqualTo(duration);
-        assertThat(aggregatedDurations.getAverageDuration()).isEqualTo(duration);
+
+        assertAggregatedDurations(aggregatedDurations, (double) duration, (double) duration, (double) duration, 1);
     }
 
     @Test
@@ -130,21 +138,21 @@ class ComputeDurationsServiceTests {
         computeDurationsService.addDurationForTask(CHAIN_TASK_ID_2, duration_2);
 
         final AggregatedDurations aggregatedDurations = computeDurationsService.getAggregatedDurations();
-        assertThat(aggregatedDurations.getMinDuration()).isEqualTo(duration_1);
-        assertThat(aggregatedDurations.getMaxDuration()).isEqualTo(duration_2);
-        assertThat(aggregatedDurations.getAverageDuration()).isEqualTo((duration_1 + duration_2) / 2.0);
+
+        assertAggregatedDurations(aggregatedDurations, (double) duration_1, (double) duration_2, (duration_1 + duration_2) / 2.0, 2);
     }
     // endregion
 
     // region Metrics evolution
     static Stream<Arguments> metricsEvolutionValuesProvider() {
-        // values, expected min, expected max, expected average
+        // values, expected min, expected max, expected average, expected count
         return Stream.of(
-                Arguments.of(List.of(), Double.NaN, Double.NaN, Double.NaN),   // No value => expected NaN
-                Arguments.of(List.of(1_000.0), 1_000.0, 1_000.0, 1_000.0),  // Single value => same expected value
-                Arguments.of(List.of(1_000.0, 500.0), 500.0, 1_000.0, 750.0), // Max as first value
-                Arguments.of(List.of(1_000.0, 2_000.0), 1_000.0, 2_000.0, 1_500.0),   // Max as second value
-                Arguments.of(List.of(1_000.0, 2_000.0, 3_000.0), 1_000.0, 3_000.0, 2_000.0) // More than 2 values
+                Arguments.of(List.of(), Double.NaN, Double.NaN, Double.NaN, 0),   // No value => expected NaN
+                Arguments.of(List.of(1_000.0), 1_000.0, 1_000.0, 1_000.0, 1),  // Single value => same expected value
+                Arguments.of(List.of(1_000.0, 500.0), 500.0, 1_000.0, 750.0, 2), // Max as first value
+                Arguments.of(List.of(1_000.0, 2_000.0), 1_000.0, 2_000.0, 1_500.0, 2),   // Max as second value
+                Arguments.of(List.of(1_000.0, 2_000.0, 3_000.0), 1_000.0, 3_000.0, 2_000.0, 3), // More than 2 values
+                Arguments.of(LONG_LIST_OF_VALUES, 1.0, (double) STATISTICS_WINDOW, (STATISTICS_WINDOW + 1) / 2.0, STATISTICS_WINDOW) // More than STATISTICS_WINDOW values
         );
     }
 
@@ -153,14 +161,40 @@ class ComputeDurationsServiceTests {
     void shouldEffectivelyTrackMetricsEvolutions(List<Double> values,
                                                  Double expectedMinValue,
                                                  Double expectedMaxValue,
-                                                 Double expectedAverageValue) {
+                                                 Double expectedAverageValue,
+                                                 long expectedCountValue) {
         for (int i = 0; i < values.size(); i++) {
             computeDurationsService.addDurationForTask(i + "", values.get(i).longValue());
         }
 
-        assertThat(minGauge.value()).isEqualTo(expectedMinValue);
-        assertThat(maxGauge.value()).isEqualTo(expectedMaxValue);
-        assertThat(averageGauge.value()).isEqualTo(expectedAverageValue);
+        assertGaugeValues(expectedMinValue, expectedMaxValue, expectedAverageValue, expectedCountValue);
+    }
+    // endregion
+
+    // region Utils
+    private void assertAggregatedDurations(AggregatedDurations aggregatedDurations,
+                                           Double expectedMinValue,
+                                           Double expectedMaxValue,
+                                           Double expectedAverageValue,
+                                           long expectedCountValue) {
+        Assertions.assertAll(
+                () -> assertThat(aggregatedDurations.getMinDuration()).isEqualTo(expectedMinValue),
+                () -> assertThat(aggregatedDurations.getMaxDuration()).isEqualTo(expectedMaxValue),
+                () -> assertThat(aggregatedDurations.getAverageDuration()).isEqualTo(expectedAverageValue),
+                () -> assertThat(aggregatedDurations.getDurationsCount()).isEqualTo(expectedCountValue)
+        );
+    }
+
+    private void assertGaugeValues(Double expectedMinValue,
+                                   Double expectedMaxValue,
+                                   Double expectedAverageValue,
+                                   long expectedCountValue) {
+        Assertions.assertAll(
+                () -> assertThat(minGauge.value()).isEqualTo(expectedMinValue),
+                () -> assertThat(maxGauge.value()).isEqualTo(expectedMaxValue),
+                () -> assertThat(averageGauge.value()).isEqualTo(expectedAverageValue),
+                () -> assertThat(countGauge.value()).isEqualTo(expectedCountValue)
+        );
     }
     // endregion
 }
