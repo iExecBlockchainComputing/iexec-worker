2b531212d722d97b29c4a0e5dacd15899079542f
Frédéric Cordier <frederic.cordier@iex.ec>
2023-12-21 10:46:38 +0100

    Remove useless constant

--- src/test/java/com/iexec/worker/compute/app/AppComputeServiceTests.java
+++ src/test/java/com/iexec/worker/compute/app/AppComputeServiceTests.java
@@ -57,8 +57,6 @@ class AppComputeServiceTests {
     private final static String DATASET_URI = "DATASET_URI";
     private final static String APP_URI = "APP_URI";
     private final static String WORKER_NAME = "WORKER_NAME";
-    private final static String TEE_POST_COMPUTE_IMAGE =
-            "TEE_POST_COMPUTE_IMAGE";
     private final static TeeSessionGenerationResponse SECURE_SESSION = mock(TeeSessionGenerationResponse.class);
     private final static long MAX_EXECUTION_TIME = 1000;
     private final static String INPUT = "INPUT";
