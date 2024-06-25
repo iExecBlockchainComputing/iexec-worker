01c0ac8b5bacebfce2201ccc9db8a1ec25111c88
Frederic CORDIER <frederic.cordier@iex.ec>
2024-06-25 09:45:47 +0200

    Set `0x0` as default value for Workerpool address

--- src/test/java/com/iexec/worker/config/SchedulerConfigurationTests.java
+++ src/test/java/com/iexec/worker/config/SchedulerConfigurationTests.java
@@ -18,11 +18,19 @@ package com.iexec.worker.config;
 
 import com.iexec.core.api.SchedulerClient;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.boot.context.annotation.UserConfigurations;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
+import org.springframework.boot.test.system.CapturedOutput;
+import org.springframework.boot.test.system.OutputCaptureExtension;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 
+@ExtendWith(OutputCaptureExtension.class)
 class SchedulerConfigurationTests {
     private final ApplicationContextRunner runner = new ApplicationContextRunner();
 
@@ -35,6 +43,19 @@ class SchedulerConfigurationTests {
                     assertThat(context).getBean("schedulerConfiguration", SchedulerConfiguration.class)
                             .extracting("poolAddress")
                             .isEqualTo("0x365E7BABAa85eC61Dffe5b520763062e6C29dA27");
+                });
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"", "0x0"})
+    void shouldFailedAndRaisedExceptionWhenPoolAddressIsInvalid(String poolAddress, CapturedOutput output) {
+        runner.withPropertyValues("core.protocol=http", "core.host=localhost", "core.port=13000", "core.poolAddress=" + poolAddress)
+                .withConfiguration(UserConfigurations.of(SchedulerConfiguration.class))
+                .run(context -> {
+                    assertThatThrownBy(() -> context.getBean(SchedulerConfiguration.class))
+                            .isInstanceOf(IllegalStateException.class)
+                            .hasCauseInstanceOf(BeanCreationException.class);
+                    assertThat(output.getOut()).contains("The workerpool address must be filled in");
                 });
     }
 }
