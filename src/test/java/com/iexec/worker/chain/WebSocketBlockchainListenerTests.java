a834dbfcfbae0b52ec2a2358f556beffcceb737c
Jeremy Bernard <jeremy.bernard@iex.ec>
2025-03-17 16:46:30 +0100

    Add annotations to disable CommandLineRunner in tests and avoid early System.exit

--- src/test/java/com/iexec/worker/chain/WebSocketBlockchainListenerTests.java
+++ src/test/java/com/iexec/worker/chain/WebSocketBlockchainListenerTests.java
@@ -23,6 +23,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.ComposeContainer;
@@ -42,6 +43,7 @@ import static org.awaitility.Awaitility.await;
 @Slf4j
 @Testcontainers
 @SpringBootTest(classes = TestApplication.class)
+@ActiveProfiles("test")
 class WebSocketBlockchainListenerTests {
 
     @Container
