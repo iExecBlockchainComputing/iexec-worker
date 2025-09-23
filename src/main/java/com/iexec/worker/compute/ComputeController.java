03c56a7188ee3c300a132a2fdcb94f152455a4c0
Natchica <nathan.gaud@gmail.com>
2025-09-23 17:40:32 +0200

    refactor: update deprecation annotation for sendExitCauseForGivenComputeStage method

--- src/main/java/com/iexec/worker/compute/ComputeController.java
+++ src/main/java/com/iexec/worker/compute/ComputeController.java
@@ -62,7 +62,7 @@ public class ComputeController {
      * @deprecated Use {@link #sendExitCausesForGivenComputeStage(String, ComputeStage, String, List)}
      *             for bulk exit cause reporting instead
      */
-    @Deprecated(since = "v9.0.1", forRemoval = true)
+    @Deprecated(forRemoval = true) // TODO: Add version when releasing next one
     @PostMapping("/compute/{stage}/{chainTaskId}/exit")
     public ResponseEntity<Void> sendExitCauseForGivenComputeStage(
             @RequestHeader("Authorization") String authorization,
